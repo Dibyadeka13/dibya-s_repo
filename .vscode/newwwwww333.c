d4318b2db26785d3c437aea9abd6ac4b0be87174
Dibyadeka13 <dibyadeka2004@gmail.com>
2025-03-16 20:21:04 +0530

    my first repo

---
+++ .vscode/newwwwww333.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+     int a,b,c;
+     printf("Enter the 1st number:");
+     scanf("%d",&a);
+ 
+         printf("Enter the 2nd number");
+     scanf("%d",&b);
+      
+     c=a*b;
+     printf("Result: %d",c);
+      
+        return 0;
+}
---
+++ new222.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+ {    
+   
+  printf("booooommmmmmmmm");
+  printf("booooommmmmmmmm");
+   printf("booooommmmmmmmm");
+  printf("booooommmmmmmmm");
+        return 0;
+     
+}
